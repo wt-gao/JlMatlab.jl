d5fe7d842b09edb334fc86f7a90666e8caa7c09b
WT-Gao <gweitao@mail.ustc.edu.cn>
2024-09-26 16:43:56 +0800

    feat(grid.jl): 优化 `ndgrid` 和 `meshgrid` 性能
    ## 性能优化
    `ndgrid` 和 `meshgrid`
    
    ## `ndgrid` 添加 `dim` 参数

--- src/grid.jl
+++ src/grid.jl
@@ -2,11 +2,12 @@
     ndgrid(x::AbstractVector...) -> Tuple{AbstractArray...}
 return `nd` carstesian product grid of input vectors in `ij` index style.
 """
-ndgrid(x::AbstractVector) = x
-ndgrid(x::AbstractVector...) = ndgrid(x)
+ndgrid(x::AbstractVector; dim=2) = dim == 1 ? (x,) : ndgrid(ntuple(i -> x, dim))
+ndgrid(x::AbstractVector...; dim=length(x)) =
+    ndgrid((x..., ntuple(i -> x[end], dim - length(x))...))
 
 function ndgrid(x::NTuple{N,AbstractVector}) where {N}
-    ntuple(i -> [v[i] for v in Iterators.product(x...)], Val(N))
+    ntuple(i -> [@inbounds v[i] for v in Iterators.product(x...)], Val(N))
 end
 
 """
@@ -16,6 +17,8 @@ return `2d` carstesian product grid of input vectors in `xy` index style.
 meshgrid(x::AbstractVector) = meshgrid(x, x)
 
 function meshgrid(x::AbstractVector, y::AbstractVector)
-    nx, ny = length(x), length(y)
-    ([x[j] for i in 1:ny, j in 1:nx], [y[i] for i in 1:ny, j in 1:nx])
+    (
+        [@inbounds v[2] for v in Iterators.product(y, x)],
+        [@inbounds v[1] for v in Iterators.product(y, x)]
+    )
 end
